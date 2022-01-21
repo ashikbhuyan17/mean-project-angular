1cc1fc073e2375afe9a7fad5d26252ca05b99b20
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2022-01-21 23:06:50 +0600

    ready to structure

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { BannerComponent } from './layout/banner/banner.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { ArticlesModule } from './articles/articles.module';
+import { StaticpagesModule } from './staticpages/staticpages.module';
+import { MiddlewareModule } from './middleware/middleware.module';
+import { AdminModule } from './admin/admin.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    PagenotfoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    ArticlesModule,
+    StaticpagesModule,
+    MiddlewareModule,
+    AdminModule,
+    AppRoutingModule,
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
